34b1149fed1167963c03cd40f41447a9d30f4d98
Vasylchenko02 <vladosvas02@gmail.com>
2021-10-12 16:02:43 +0300

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi, World!");
+        String nameOfPlanet = "Earth";
+        System.out.println("Our planet has name " + nameOfPlanet);
     }
 }
